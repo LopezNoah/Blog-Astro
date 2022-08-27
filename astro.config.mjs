26edb1f813344683b9486e1d2357dcb56e5f6f3b
nlopez4 <noahtheark2@gmail.com>
2022-08-27 07:18:52 +0000

    Add mdx and remove fastify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
-//import node from '@astrojs/node';
+import { defineConfig } from 'astro/config'; //import node from '@astrojs/node';
+
 import deno from '@astrojs/deno';
 import tailwind from '@astrojs/tailwind';
 import solidJs from "@astrojs/solid-js";
-
 import compress from "astro-compress";
+import prefetch from "@astrojs/prefetch";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), compress()],
+  integrations: [tailwind(), solidJs(), compress(), prefetch(), mdx()],
   output: 'server',
   adapter: deno()
 });
