a8efcfe4da13cb18346b4b87e07059bdef7e99a8
nlopez4 <noahtheark2@gmail.com>
2022-08-30 05:03:04 +0000

    Add critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
 import mdx from "@astrojs/mdx";
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), compress(), prefetch(), mdx()],
+  integrations: [tailwind(), solidJs(), compress(), prefetch(), mdx(), critters()],
   output: 'server',
   adapter: deno()
 });
