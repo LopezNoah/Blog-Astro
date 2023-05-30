8cee15f0398200f72c43bd32e28255806143d30c
nlopez4 <noahtheark2@gmail.com>
2023-05-29 18:37:38 -0700

    Push vercel server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,15 @@
 import { defineConfig } from "astro/config"; //import node from '@astrojs/node';
-
-import deno from "@astrojs/deno";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
 import mdx from "@astrojs/mdx";
 
-//This does not change the values in the site. I'll remove this later
-import critters from "astro-critters";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    solidJs(),
-    compress(),
-    prefetch(),
-    mdx(),
-    critters(),
-  ],
+  integrations: [tailwind(), solidJs(), compress(), prefetch(), mdx()],
   output: "server",
-  adapter: deno(),
+  adapter: vercel()
 });
