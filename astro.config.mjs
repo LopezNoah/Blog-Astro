9f752a8d9ebb541b58b7ba08a90a5d50f3fc52ec
nlopez4 <noahtheark2@gmail.com>
2023-08-06 20:45:58 -0700

    Remove unused packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config"; //import node from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
 import mdx from "@astrojs/mdx";
 
@@ -9,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), compress(), prefetch(), mdx()],
+  integrations: [tailwind(), solidJs(), prefetch(), mdx()],
   output: "server",
   adapter: vercel()
 });
